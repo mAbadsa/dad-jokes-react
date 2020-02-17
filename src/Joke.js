b51f737b67cb91312dff253a33a912ea6851450f
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-02-17 17:23:28 +0200

    Adding Dynamic Colors and Emojis

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,72 @@
-import React, { Component } from 'react';
-import './Joke.css';
+import React, { Component } from "react";
+import "./Joke.css";
 
-class Joke extends Component {   
-    // handleVote() {
-    //     this.props.handleVote(this.props.id, 1);
-    // } 
-    render() {
-        return (
-            <div className="Joke">
-                <div className="Joke-buttons">
-                    <i className="fas fa-arrow-up" onClick={this.props.upVote}></i>
-                    <span className="Joke-votes">{this.props.votes}</span>
-                    <i className="fas fa-arrow-down" onClick={this.props.downVote}></i>
-                </div>
-                <div className="Joke-text">
-                    {this.props.joke}
-                </div>
-                <div className="Joke-smiley">
-                    <i className="em em-rolling_on_the_floor_laughing"></i>
-                </div>
-            </div>
-        );
+class Joke extends Component {
+  static defaultProps = {
+    colors: ["#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#f44330"],
+    emoje: [
+      "em em-rolling_on_the_floor_laughing",
+      "em-laughing",
+      "em-smiley",
+      "em-slightly_smiling_face",
+      "em-neutral_face",
+      "em-confused",
+      "em-angry"
+    ]
+  };
+  getColor() {
+    if (this.props.votes >= 15) {
+      return "#4caf50";
+    } else if (this.props.votes >= 12) {
+      return "#8bc34a";
+    } else if (this.props.votes >= 9) {
+      return "#cddc39";
+    } else if (this.props.votes >= 6) {
+      return "#ffeb3b";
+    } else if (this.props.votes >= 3) {
+      return "#ffc107";
+    } else if (this.props.votes >= 0) {
+      return "#ff9800";
+    } else {
+      return "#f44330";
     }
+  }
+
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.votes >= 12) {
+      return "em em-laughing";
+    } else if (this.props.votes >= 9) {
+      return "em em-smiley";
+    } else if (this.props.votes >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.votes >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.votes >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  }
+
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-buttons">
+          <i className="fas fa-arrow-up" onClick={this.props.upVote}></i>
+          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
+            {this.props.votes}
+          </span>
+          <i className="fas fa-arrow-down" onClick={this.props.downVote}></i>
+        </div>
+        <div className="Joke-text">{this.props.joke}</div>
+        <div className="Joke-smiley">
+          <i className={this.getEmoji()}></i>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Joke;
