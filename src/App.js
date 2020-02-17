f76e2fb53f3e1d3197bfc0ad559335aed0643a91
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-02-17 13:17:04 +0200

    Add Joke Component and render jokesList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import JokeList from './JokeList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <JokeList />
     </div>
   );
 }
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,13 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Joke from "./Joke";
+import axios from "axios";
 
 class JokeList extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Dad Jokes</h1>
-            </div>
-        );
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      jokes: [],
+      isLoading: false
+    };
+  }
+
+  async componentDidMount() {
+    const JOKES_URL = "https://icanhazdadjoke.com/";
+    const jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      const res = await axios.get(JOKES_URL, {
+        headers: { Accept: "application/json" }
+      });
+      jokes.push(res.data.joke);
     }
+    if (jokes.length !== 10) {
+      console.log("1", jokes.length);
+      console.log(this.state.isLoading);
+      this.setState((prevProps, prevState) => ({
+        isLoading: !prevState.isLoading
+      }));
+    } else {
+      console.log("2", jokes.length);
+      this.setState(st => ({
+        jokes: [...st.jokes, ...jokes]
+      }));
+    }
+    console.log(jokes);
+  }
+  render() {
+    const jokeList = this.state.jokes.map(joke => (
+      <Joke key={joke} joke={joke} />
+    ));
+    return (
+      <div className="JokeList">
+        {this.state.isLoading ? (
+          <div>
+            <h5>Loading...</h5>
+          </div>
+        ) : (
+          <div>
+            <h1>Dad Jokes</h1>
+            <div className="JokeList-jokes">{jokeList}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default JokeList;
